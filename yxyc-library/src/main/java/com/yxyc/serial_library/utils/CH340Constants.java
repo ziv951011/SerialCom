3e645fe90d1d0c8154cfc1f8aedc9774a7d16be8
ziv <ziv951011@gmail.com>
2018-07-28 13:56:37 +0800

    新增接收成功与失败的常量

---
+++ yxyc-library/src/main/java/com/yxyc/serial_library/utils/CH340Constants.java
@@ -0,0 +1,9 @@
+package com.yxyc.serial_library.utils;
+
+public class CH340Constants {
+
+    // 成功的数据
+    public static final Integer SUCCESS_DATA = 0X11;
+    // 失败的数据
+    public static final Integer  FILD_DATA = 0X12;
+}
